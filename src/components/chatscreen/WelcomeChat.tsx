a3e16bd3ba9835c996aa9119d428160ece348a27
Jhayrolandero <xjaylandero@gmail.com>
2024-11-30 23:14:31 +0800

    init

--- src/components/chatscreen/WelcomeChat.tsx
+++ src/components/chatscreen/WelcomeChat.tsx
@@ -1,9 +1,18 @@
 import { IoSend } from "react-icons/io5";
+import { TypewriterEffect } from "../ui/Typewriter";
+import Typewriter from "../Typewriter";
 
 const WelcomeChat = () => {
   return (
     <div className="max-w-[480px] gap-4">
-    <p className=" text-center text-[1.5rem]">Hello, how can i assist you today?</p>
+        <p className="text-center text-[1.5rem] font-semibold">
+          {
+          Typewriter({
+            text: "Hello, how can i assist you today?",
+            delay: 50
+            })
+          }
+        </p>
     <div className="bg-[#141618] flex justify-between items-center px-4 rounded-full">
     <input type="text" className="w-[480px] h-[40px] bg-transparent focus:outline-none border-0"/>
     <button>
